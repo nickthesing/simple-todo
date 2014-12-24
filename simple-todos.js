f9c95b348e5a0088515372aac7017392ec5c3a89
nickthesing <nickthesing@gmail.com>
2014-12-24 11:44:29 +0100

    First Init

---
+++ simple-todos.js
@@ -0,0 +1,41 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+    Template.body.helpers({
+        tasks: function() {
+            if ( Session.get("hideCompleted") ) {
+                return Tasks.find({checked: {$ne: true}}, {sort: {createdat: -1}});
+            } else {
+
+            }
+            return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+    })
+
+    Template.body.events({
+        "submit .new-task": function(event) {
+
+            var text = event.target.text.value;
+
+            if ( text === '' ) return;
+
+            Tasks.insert({
+                text: text,
+                createdAt: new Date()
+            });
+
+            event.target.text.value = '';
+
+            return false;
+        },
+        "click .toggle-checked": function() {
+            Tasks.update(this._id, {$set: {checked: ! this.checked }});
+        },
+        "click .delete": function() {
+            Tasks.remove(this._id);
+        },
+        "change .hide-completed input": function(event) {
+            Session.set("hideCompleted", event.target.checked);
+        },
+    });
+}
